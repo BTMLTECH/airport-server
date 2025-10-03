7bae5c8006452e89a38732ff70d09eebcef8a946
BTMLTECH <btmltechnology@gmail.com>
2025-10-03 12:06:49 +0100

    new env

--- src/server.ts
+++ src/server.ts
@@ -13,9 +13,10 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 
 const allowedOrigins = [
-  "http://localhost:8080",
-  "http://localhost:8081",
-  // process.env.FRONTEND_URL!,
+  // "http://localhost:8080",
+  // "http://localhost:8081",
+  process.env.FRONTEND_LOGBOOK!,
+  process.env.FRONTEND_PROTOCOL!,
 ];
 app.use(
   cors({
