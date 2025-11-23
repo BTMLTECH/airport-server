b7cc30790ac52cd198f6661655893384e44395cc
BTMLTECH <technology@btmlimited.net>
2025-11-23 11:52:23 +0100

    server congoif

--- src/app.ts
+++ src/app.ts
@@ -11,11 +11,14 @@ app.use(bodyParser.json());
 app.use(
   cors({
     origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-           const allowedOrigins = [
-        // "http://localhost:8080",
-        // FRONTEND,
+      if (!origin) return callback(null, true); // allow Postman or server requests
+      const allowedOrigins = [
         "https://protocol.btmtravel.net",
+        "http://protocol.btmtravel.net",
+        "http://51.75.154.196",
+        "http://51.75.154.196:8080",
+        // "http://localhost:5173",
+        // "http://localhost:8080",
       ];
       if (allowedOrigins.includes(origin)) return callback(null, true);
       return callback(new Error(`Origin ${origin} not allowed by CORS`), false);
@@ -23,5 +26,6 @@ app.use(
     credentials: true,
   })
 );
+
 
 export default app;
