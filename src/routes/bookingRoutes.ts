4a33bc5ef8bd3c61a81f331fbeed6d9f0220fde7
BTMLTECH <technology@btmlimited.net>
2026-02-25 11:10:54 +0100

    updated server leave reliver least2

--- src/routes/bookingRoutes.ts
+++ src/routes/bookingRoutes.ts
@@ -7,7 +7,15 @@ import { traacBooking } from "../controllers/traacBoongController";
 const router = Router();
 
 router.post("/booking", bookingController);
-router.post("/traacbooking", uploadDocument.single("file"), traacBooking);
+// router.post("/traacbooking", uploadDocument.single("file"), traacBooking);
+router.post("/traacbooking", (req, res, next) => {
+  const contentType = req.headers["content-type"];
+  if (contentType && contentType.includes("multipart/form-data")) {
+    uploadDocument.single("file")(req, res, next);
+  } else {
+    next();
+  }
+}, traacBooking);
 router.get("/verify-payment", verifyPayment);
 
 
